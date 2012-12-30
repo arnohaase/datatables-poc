9b39b64aa32e8d3772091c40f583dae7848c4344
Arno Haase <arno.haase@haase-consulting.com>
2012-12-30 23:54:33 +0100

    added servlet filter to simulate network latency

---
+++ server/src/main/java/de/arnohaase/datatables/util/LatencyServletFilter.java
@@ -0,0 +1,33 @@
+package de.arnohaase.datatables.util;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+
+import org.apache.log4j.Logger;
+
+
+public class LatencyServletFilter implements Filter {
+    public static final int LATENCY_MILLIS = 300; //TODO make this configurable
+    
+    private static final Logger log = Logger.getLogger(LatencyServletFilter.class);
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            log.info("delaying request for " + LATENCY_MILLIS + "ms.");
+            Thread.sleep(LATENCY_MILLIS);
+            chain.doFilter(request, response);
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
