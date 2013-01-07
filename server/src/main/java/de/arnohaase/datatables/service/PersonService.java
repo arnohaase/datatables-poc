e4589ea2e408226d1b7ac647aec0b28551addebc
Thomas Greve <tom@greve.islay.local>
2013-01-07 19:30:15 +0100

    Some real Randomness + German Umlauts

--- server/src/main/java/de/arnohaase/datatables/service/PersonService.java
+++ server/src/main/java/de/arnohaase/datatables/service/PersonService.java
@@ -1,8 +1,14 @@
 package de.arnohaase.datatables.service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
@@ -11,40 +17,82 @@ import de.arnohaase.datatables.model.Person;
 import de.arnohaase.datatables.model.Sex;
 import de.arnohaase.datatables.util.GenericCloner;
 
-
 public class PersonService {
     private static final Logger log = Logger.getLogger(PersonService.class);
-    
+
+    static final char[] UPPERCASE = new char[29];
+    static final char[] LOWERCASE = new char[30];
+    static final long _01011960 = new GregorianCalendar(1960, Calendar.JANUARY, 1).getTimeInMillis();
+    static final int PERIOD = (30 * 365 + 8) * 86400; // 30 a
+
+    static final Random rand = new Random();
+
     private static final Map<Integer, Person> persons = new TreeMap<Integer, Person>();
     private static int nextOid=0;
-    
-    private static final int INITIAL_NUM_PERSONS = 1000;
-    
-    static {
-        for (int i=0; i<INITIAL_NUM_PERSONS; i++) {
-            final Person p = new Person();
-            
-            p.setOid(createNewOid());
-            p.setFirstname("Vorname-" + i);
-            p.setLastname("Nachname-" + i);
-            p.setStreet("Straße " + i);
-            p.setZip("12345");
-            p.setCity("Stadt " + i);
-            switch(i%3) {
-            case 0: p.setCountry("Deutschland");break;
-            case 1: p.setCountry("Great Britain");break;
-            case 2: p.setCountry("France");break;
-            }
-            p.setLocale(getLocale(i));
-            p.setBirthday(String.format("%4d-%02d-%02d", 1960+i%30, i%12 + 1, i%10 + 10));
-            p.setSex(i%4 < 2 ? Sex.m : Sex.f);
-            p.setIncome(40000 + (i*123471 % 8000000) /100.0);
 
+    private static final int INITIAL_NUM_PERSONS = 1000;
+
+    static {
+    	for (char c = 'A'; c <= 'Z'; c++)
+    		UPPERCASE[c - 'A'] = c;
+    	System.arraycopy(new char[] { 'Ä',  'Ö',  'Ü' },  0,  UPPERCASE, 26, 3);
+    	for (char c = 'a'; c <= 'z'; c++)
+    		LOWERCASE[c - 'a'] = c;
+    	System.arraycopy(new char[] { 'ä',  'ö',  'ü', 'ß' },  0,  LOWERCASE, 26, 4);
+
+        for (int i=0; i<INITIAL_NUM_PERSONS; i++) {
+            Person p = createPerson(i);
             persons.put(p.getOid(), p);
         }
     }
 
-    private static String getLocale(int i) {
+    static DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	static Person createPerson(int i) {
+		Person p = new Person();
+
+		p.setOid(createNewOid());
+		p.setFirstname(randomString(8));
+		p.setLastname(randomString(10));
+		p.setStreet(randomString(10) + "straße " + rand.nextInt(100));
+		p.setZip(String.format("%05d", rand.nextInt(100000)));
+		p.setCity(randomString(6) + "stadt");
+		switch(i%3) {
+		case 0: p.setCountry("Deutschland"); break;
+		case 1: p.setCountry("Great Britain"); break;
+		case 2: p.setCountry("France"); break;
+		}
+		p.setLocale(getLocale(i));
+		p.setBirthday(format.format(new Date(_01011960 + rand.nextInt(PERIOD) * 1000L)));
+		p.setSex(randomEnum(Sex.class));
+		p.setIncome(40000 + i*123471 % 8000000 / 100.0);
+		return p;
+	}
+
+	/**
+	 * @param len unverbindliche Wunschlänge
+	 */
+	static String randomString(int len) {
+		len += rand.nextInt() % len / 3; // +/- 33%
+		StringBuilder sb = new StringBuilder(len);
+		sb.append(randomUppercase());
+		for (int i = 1; i < len; i++)
+			sb.append(randomLowercase());
+		return sb.toString();
+	}
+
+	static char randomUppercase() {
+		return UPPERCASE[rand.nextInt(UPPERCASE.length)];
+	}
+	static char randomLowercase() {
+		return LOWERCASE[rand.nextInt(LOWERCASE.length)];
+	}
+
+	static <E extends Enum<E>> E randomEnum(Class<E> clazz) {
+		E[] values = clazz.getEnumConstants();
+		return values[rand.nextInt(values.length)];
+	}
+
+	private static String getLocale(int i) {
         switch(i%6) {
         case 0: return "de_DE";
         case 1: return "de_AT";
@@ -56,10 +104,10 @@ public class PersonService {
     }
 
     public static PersonService INSTANCE = new PersonService();
-    
+
     private PersonService() {
     }
-    
+
     public synchronized int getNumPersons() {
         return persons.size();
     }
@@ -67,7 +115,7 @@ public class PersonService {
     private static int createNewOid() {
         return nextOid++;
     }
-    
+
     public synchronized void insert(Person p) {
         if(p.getOid() != null) {
             throw new IllegalArgumentException("was already inserted");
@@ -75,7 +123,7 @@ public class PersonService {
         p.setOid(createNewOid());
         persons.put(p.getOid(), GenericCloner.clone(p));
     }
-    
+
     public synchronized void update(Person p) {
         System.out.println("!!!!!!!!!!!!!!!!!!");
         log.info("updating person " + p.getOid());
@@ -91,10 +139,10 @@ public class PersonService {
         }
         persons.remove(oid);
     }
-    
+
 	public synchronized List<Person> findPersons(int offset, int maxNum) {
 		final List<Person> result = new ArrayList<Person>();
-		
+
 		int count=0;
 		for (Person p: persons.values()) {
 		    if(count >= offset) {
@@ -105,7 +153,7 @@ public class PersonService {
 		        break;
 		    }
 		}
-		
+
 		return result;
 	}
 
