3685ca2bd48568a281e1952019cb00d2c0d7542f
Thomas Greve <tom@greve.islay.local>
2013-01-10 22:35:27 +0100

    Country vereinfacht, pngs -> a-x

--- server/src/main/java/de/arnohaase/datatables/model/Country.java
+++ server/src/main/java/de/arnohaase/datatables/model/Country.java
@@ -1,38 +1,5 @@
 package de.arnohaase.datatables.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum Country {
-    DEUTSCHLAND, GROSSBRITANNIEN("Großbritannien", "GB"), FRANKREICH;
-
-    String _name;
-    String _iso3166;
-
-    Country() {
-        _name = name().charAt(0) + name().substring(1).toLowerCase();
-        _iso3166 = name().substring(0, 2);
-    }
-    Country(String name, String iso3166) {
-        _name = name;
-        _iso3166 = iso3166;
-    }
-
-    public String getName() {
-        return _name;
-    }
-
-    public String getIso3166() {
-        return _iso3166;
-    }
-
-    static Map<String, Country> _byIso3166;
-    static Country byIso3166(String iso3166) {
-        if (_byIso3166 == null) {
-            _byIso3166 = new HashMap<String, Country>();
-            for (Country c : values())
-                _byIso3166.put(c._iso3166, c);
-        }
-        return _byIso3166.get(iso3166);
-    }
+    AD, AT, BE, CH, CZ, DE, DK, EE, ES, FR, GB, GR, IE, IS, IT, LT, LU, LV, NL, MC, NO, PL, PT, SE;
 }
